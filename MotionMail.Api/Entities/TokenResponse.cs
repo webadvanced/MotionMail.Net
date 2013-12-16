34e690559f155070b6eab1f847d1975b644a8bb8
Web Advanced <paul.barton@webadvanced.com>
2013-12-16 11:43:31 -0800

    added MotionMailConfiguration file

---
+++ MotionMail.Api/Entities/TokenResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace MotionMail.Api.Entities {
+    using System;
+
+    public class TokenResponse {
+        #region Public Properties
+
+        public DateTimeOffset DateCreated { get; set; }
+
+        public bool Success { get; set; }
+
+        public string Value { get; set; }
+
+        #endregion
+    }
+}
---
+++ MotionMail.Api/Infrastructure/MotionMailConfiguration.cs
@@ -0,0 +1,42 @@
+﻿namespace MotionMail.Api.Infrastructure {
+    using System;
+    using System.Configuration;
+
+    public static class MotionMailConfiguration {
+        #region Constants and Fields
+
+        private static string apiKey;
+
+        private static string secretKey;
+
+        #endregion
+
+        #region Methods
+
+        internal static string GetApiKey() {
+            if (String.IsNullOrEmpty(apiKey)) {
+                apiKey = ConfigurationManager.AppSettings["MotionMailApiKey"];
+            }
+
+            return apiKey;
+        }
+
+        internal static string GetSecretKey() {
+            if (String.IsNullOrEmpty(secretKey)) {
+                apiKey = ConfigurationManager.AppSettings["MotionMailSecretKey"];
+            }
+
+            return apiKey;
+        }
+
+        internal static void SetApiKey(string key) {
+            apiKey = key;
+        }
+
+        internal static void SetSecretKey(string key) {
+            secretKey = key;
+        }
+
+        #endregion
+    }
+}
